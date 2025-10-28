5ef3b7b9c9aca9a8eda743fddcb52ad799856192
AlaricTR <alaric.tru@gmail.com>
2025-10-28 16:30:07 -0300

    added itemSelect functionnality; populate it with items from outputs array of JS objects.

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,59 @@
-alert("hello world");
+
+const outputs =  {
+    Lance36: 
+    [
+            {
+                recipeNumber : 1,
+                output : "lance-36",
+                tier : 4, 
+                outputAmount : 1,
+                inputs : 
+                [
+                    {material : "steel", amount : 50},
+                    {material : "asm5", amount : 35},
+                    {material : "asm4", amount : 60},
+                    {material : "asm3", amount : 35}
+                    
+                ]
+            },
+            {
+                recipeNumber : 2,
+                output : "lance-36",
+                tier : 4, 
+                outputAmount : 1,
+                inputs : 
+                [
+                    {material : "steel", amount : 50},
+                    {material : "asm5", amount : 35},
+                    {material : "asm4", amount : 60},
+                    {material : "asm3", amount : 35}
+                    
+                ]
+            }
+    ]
+
+         
+}
+window.onload = () => {
+    
+    
+    populateOutputSelect(outputs);
+}
+
+function populateOutputSelect(outputs) 
+{
+    const itemSelect = document.getElementById("item-select");
+    itemSelect.innerHTML = '<option value="">--Select--</option>';
+    Object.keys(outputs)
+        .filter(key => outputs[key][0].tier > 0)
+        .forEach(key =>
+        {
+            const opt = document.createElement("option");
+            opt.value = key;
+            opt.textContent = outputs[key][0].output;
+            itemSelect.appendChild(opt);
+        }
+        );
+        
+        
+}
---
+++ scripts/outputs.js
@@ -0,0 +1,20 @@
+const outputs = {
+    Lance36: 
+    [
+        {
+            recipeNumber : 1,
+            output : "lance-36",
+            tier : 4, 
+            outputAmount : 1,
+            inputs : 
+            [
+                {material : "steel", amount : 50},
+                {material : "asm5", amount : 35},
+                {material : "asm4", amount : 60},
+                {material : "asm3", amount : 35}
+                
+            ]
+
+        }
+    ]
+}
